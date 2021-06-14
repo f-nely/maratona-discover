7b3d56e050038683dd91877a851dfb712a4dd6cb
nely de faria <nelly.nf54@gmail.com>
2021-06-14 12:49:32 -0300

    Adicionando eventos vi js

--- maratona-discover/javascript/main.js
+++ maratona-discover/javascript/main.js
@@ -5,6 +5,25 @@ function print() {
 
 const input = document.querySelector("input");
 
+// eventos via teclado
 input.onkeydown = function() {
     console.log("rodei");
+}
+
+// adicionando eventos via js
+const li = document.querySelector("li");
+
+li.addEventListener('mouseover', print);
+
+/* li.onclick = function() {
+    console.log("Hey you!");
+} */
+
+li.addEventListener('dblclick', function() {
+    console.log("Outro momento!");
+})
+
+// argumento event
+input.onkeydown = function(event) {
+    console.log(event.key);
 }
